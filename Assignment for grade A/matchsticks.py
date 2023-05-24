2021b38cbfbd5189f8eabcbdff690676767300ac
Jonathan <123389765+NeguseNegest@users.noreply.github.com>
2023-05-24 12:48:31 +0200

    Update matchsticks.py

--- Assignment for grade A/matchsticks.py
+++ Assignment for grade A/matchsticks.py
@@ -1,24 +1,26 @@
 
-
 # Function to calculate smallest possible number with n matchsticks
 def get_min(n):
     # Initialize the list with None for all indices
-    dp = [None for _ in range(n+1)]
+    if n<2 or n>100:
+        raise ValueError('Invalid input: n must be within bound 2<=n<=100.')
+
+    computed_numbers = [None for _ in range(n+1)]
   
     # The first 21 values are known directly.
     # Here, I'm using Python's zero-based indexing, so there's an offset of 2
     smallest_nums = [1, 7, 4, 2, 6, 8, 10, 18, 22, 20, 28, 68, 88, 108, 188, 200, 208, 288, 688, 888, 1088, 1888]
 
+
     for i in range(2, n+1):
         if i < len(smallest_nums):
             # We already know the answer for this
-            dp[i] = str(smallest_nums[i-2])
+            computed_numbers[i] = str(smallest_nums[i-2])
         else:
             # Otherwise, calculate it based on previously known values
-            dp[i] = dp[i-2] + '1' if 21 <= i <= 23 and int(dp[i-2] + '1') < int(dp[i-7] + '8') else dp[i-7] + '8'
-
+            computed_numbers[i] = computed_numbers[i-2] + '1' if 21 <= i <= 23 and int(computed_numbers[i-2] + '1') < int(computed_numbers[i-7] + '8') else computed_numbers[i-7] + '8'
     # Return the result for n
-    return dp[n]
+    return computed_numbers[n]
 
 
 
@@ -31,30 +33,31 @@ def get_max(n):
         return "1" * (n // 2)
 
 
-# Function to precomputed_20ve a single case
-def precomputed_20ve_case(n):
-    smallest = get_min(n)
-    largest = get_max(n)
+# Function to get smallest & largest number possible
+def compute(n):
+    smallest = get_min(n) #return the smallest
+    largest = get_max(n) #return the largest
     return f"{smallest} {largest}"
 
 # Main function
 def main():
     # Number of test cases
-    t = int(input())
+    val = int(input())
     cases = []
 
     # Read all test cases
-    for _ in range(t):
+    for _ in range(val):
         n = int(input())
         cases.append(n)
 
-    # precomputed_20ve all test cases
-    results = [precomputed_20ve_case(n) for n in cases]
+    results = [compute(n) for n in cases]
 
     # Print the results
     for result in results:
         print(result)
 
-# Entry point of the script
+    
+
 if __name__ == "__main__":
     main()
+
